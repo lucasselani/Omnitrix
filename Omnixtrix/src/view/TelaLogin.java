2aeb7a7765f06827963aa0780dc6c23b7b362252
Lucas Selani de Melo <lucas_selani@hotmail.com>
2016-10-31 16:21:08 -0200

    Initial Project

---
+++ Omnixtrix/src/view/TelaLogin.java
@@ -0,0 +1,22 @@
+package view;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class TelaLogin extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public TelaLogin(Composite parent, int style) {
+		super(parent, style);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+}
